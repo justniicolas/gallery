1479d89d141f3715ea3d2dbf43584e1e9d1d394c
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-08-28 01:32:45 +0200

    ⚙️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -7,8 +6,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel({
-    analytics: true
-  })
-});
+  });
