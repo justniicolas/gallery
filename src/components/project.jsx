39bcd3d180404c783c43f937b0f4b1b5eacf614e
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-08-28 00:59:31 +0200

    Update project.jsx

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
   const [recentRepositories, setRecentRepositories] = useState([]);
 
   useEffect(() => {
-
+    const accessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN
 
     const fetchDataWithRetry = async (url, config, retries = 0) => {
       try {
